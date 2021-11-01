258f049e23d101134a5a11e9e2ab040eeef43451
abdullah <pgcbwp12@gmail.com>
2021-11-01 23:33:21 +0500

    home delete

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:sci_fi/dashboard.dart';
-import 'package:sci_fi/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const DashBorad(),
     );
   }
 }
